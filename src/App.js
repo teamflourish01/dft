2d182b9ed7bab46292522c1aa74069428e01b8c5
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-07-16 17:30:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        {/* <Route path='/' element={<Home/>}/> */}
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import "../footer/Footer.css";
+import fimg from "../../images/fimg.png";
+import flogo from "../../images/flogo.png"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="fimg">{/* <img src={fimg} alt="" srcset="" /> */}</div>
+      <div className="footer-main">
+        <div className="under-1320">
+          <div className="under-1026">
+          <div className="logoimg">
+            <img src={flogo} alt="" srcset="" />
+          </div>
+          <div className="since">
+            <p>Since : 1983</p>
+            <p>DFT Alumni Family, Bhavnagar</p>
+          </div>
+          <div className="link">
+          <div className="link-flex">
+            <li>Home</li>
+            <li>About DFT Alumni</li>
+            <li>Newsletter</li>
+            <li>Notable Alumni</li>
+            <li>Gallery</li>
+            <li>Contact us</li>
+            </div>
+       
+          </div>
+          <div className="f-link">
+            <p>All Rights Reserved DFT Alumni Family | Developed By: Flourish Creations Private Limited</p>
+          </div>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
