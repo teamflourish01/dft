4756e8de57d192bbb2931404c60eb3f8e8a79bd6
Prince Mendpara <prince.m.7405@gmail.com>
2024-07-16 13:21:14 +0530

    folder structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
