adcaedb6b9af65bcd315a20b1baeee8db4f885c5
RinaBhalani <rina.teamflourish@gmail.com>
2024-07-16 16:23:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
       <Routes>
+    
         <Route path='/' element={<Home/>}/>
       </Routes>
     </div>
