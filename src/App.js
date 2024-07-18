22878595288177d5e8916de0e8022b3e9956ba02
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-07-18 18:35:25 +0530

    commit footer testimoniam home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-        {/* <Route path='/' element={<Home/>}/> */}
+        <Route path='/' element={<Home/>}/>
+        
+
       </Routes>
       <Footer/>
     </div>
