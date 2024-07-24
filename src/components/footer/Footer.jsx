2c5a83a90a8bdb3ed47c6b11daa2b6bee8f00454
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-07-24 11:31:22 +0530

    about li

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useLocation } from 'react-router-dom';
 import "../footer/Footer.css";
 import fimg from "../../images/fimg.png";
-import flogo from "../../images/flogo.png"
+import flogo from "../../images/flogo.png";
+
 const Footer = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       {/* <div className="fimg"><img src={fimg} alt="" srcset="" className="fimg-end" /> </div> */}
       <div className="footer-padding">
-      <div className="footer-main">
-        <div className="under-1320">
-          <div className="under-1026">
-          <div className="logoimg">
-            <img src={flogo} alt="" srcset="" />
-          </div>
-          <div className="since">
-            <p>Since : 1983</p>
-            <p>DFT Alumni Family, Bhavnagar</p>
-          </div>
-          <div className="link">
-          <div className="link-flex">
-            <li>Home</li>
-            <li>About DFT Alumni</li>
-            <li>Newsletter</li>
-            <li>Notable Alumni</li>
-            <li>Gallery</li>
-            <li>Contact us</li>
+        <div className="footer-main">
+          <div className="under-1320">
+            <div className="under-1026">
+              <div className="logoimg">
+                <img src={flogo} alt="" srcSet="" />
+              </div>
+              <div className="since">
+                <p>Since : 1983</p>
+                <p>DFT Alumni Family, Bhavnagar</p>
+              </div>
+              <div className="link">
+                <div className="link-flex">
+                  <Link to="/" className="footer-link">
+                    <li>Home</li>
+                  </Link>
+                  <li>
+                    <Link to="/about" className="nav-link">About DFT Alumni</Link>
+                  </li>
+                  <li>
+                    <Link to="/newsletter" className="nav-link">Newsletter</Link>
+                  </li>
+                  <li>
+                    <Link to="/notabalalumni" className="nav-link">Notable Alumni</Link>
+                  </li>
+                  <li>
+                    <Link to="/Gallerypage" className="nav-link">Gallery</Link>
+                  </li>
+                  <li>
+                    <Link to="/contactus" className="nav-link">Contact Us</Link>
+                  </li>
+                </div>
+              </div>
+              <div className="f-link">
+  <p>All Rights Reserved DFT Alumni Family | Developed By: 
+    <a href="https://teamflourish.co/" target="_blank" rel="noopener noreferrer" className="devlopedby">
+      Flourish Creations Private Limited
+    </a>
+  </p>
+</div>
+
             </div>
-       
           </div>
-          <div className="f-link">
-            <p>All Rights Reserved DFT Alumni Family | Developed By: Flourish Creations Private Limited</p>
-          </div>
-            </div>
         </div>
-      </div>
       </div>
     </>
   );
