30651268de67e360d42ef015df4027d0cbad33b3
RinaBhalani <rina.teamflourish@gmail.com>
2024-07-25 17:35:58 +0530

    commit notable changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,113 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import '../navbar/Navbar.css';
+// import logo from "../../images/dftlogo.png"
+// import Hamburger from 'hamburger-react'
+// import { Link } from 'react-router-dom';
+
+// const Navbar = () => {
+//   const [isOpen, setOpen] = useState(false);
+
+//   const closeMenu = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <section>
+//       <div className='full-width'>
+
+//         <div className="main-content-1320">
+//           <div className="padding-dft">
+//             <div className="main-navbar">
+//               <div className="inner-navbar">
+//                 <div className='dft-logo-hamburger'>
+//                 <Link to="/" className="nav-link">
+//                   <img src={logo} alt="Logo" />
+//                   </Link>
+//                 </div>
+//                 <ul className={`navigation-bar ${isOpen ? 'open' : ''}`}>
+//                   <li>
+//                     <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/about" className="nav-link" onClick={closeMenu}>About DFT Alumni</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/newsletter" className="nav-link" onClick={closeMenu}>Newsletter</Link>
+//                   </li>
+//                   <div className='dft-logo'>
+//                     <Link to="/" className="nav-link">
+//                       <img src={logo} alt="Logo" />
+//                     </Link>
+//                   </div>
+//                   <li>
+//                     <Link to="/notabalalumni" className="nav-link" onClick={closeMenu}>Notabal Alumni</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/Gallerypage" className="nav-link" onClick={closeMenu}>Gallery</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/contactus" className="nav-link" onClick={closeMenu}>Contact Us</Link>
+//                   </li>
+//                 </ul>
+//                 <div className="hamburger-menu">
+//                   <Hamburger toggled={isOpen} toggle={setOpen} />
+//                 </div>
+//               </div>
+//             </div>
+
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Navbar;
+
+
+
+import React, { useState, useEffect, useRef } from 'react';
 import '../navbar/Navbar.css';
-import logo from "../../images/dftlogo.png"
-import Hamburger from 'hamburger-react'
+import logo from "../../images/dftlogo.png";
+import Hamburger from 'hamburger-react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isOpen, setOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const closeMenu = () => {
     setOpen(false);
   };
 
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      closeMenu();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <section>
-      <div className='full-width'>
-    
+      <div className='full-width shadow-navbar'>
         <div className="main-content-1320">
           <div className="padding-dft">
             <div className="main-navbar">
-              <div className="inner-navbar">
+              <div className="inner-navbar" ref={menuRef}>
                 <div className='dft-logo-hamburger'>
-                  
-                  <img src={logo} alt="Logo" />
+                  <Link to="/" className="nav-link">
+                    <img src={logo} alt="Logo" />
+                  </Link>
                 </div>
                 <ul className={`navigation-bar ${isOpen ? 'open' : ''}`}>
                   <li>
@@ -35,8 +120,8 @@ const Navbar = () => {
                     <Link to="/newsletter" className="nav-link" onClick={closeMenu}>Newsletter</Link>
                   </li>
                   <div className='dft-logo'>
-                  <Link to="/" className="nav-link">
-                    <img src={logo} alt="Logo" />
+                    <Link to="/" className="nav-link">
+                      <img src={logo} alt="Logo" />
                     </Link>
                   </div>
                   <li>
@@ -54,7 +139,6 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
