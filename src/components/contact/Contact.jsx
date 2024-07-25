2741777a53eb3ba6c7145505c0ae2b32c3a3e2b0
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-07-25 18:15:30 +0530

    commit all 25-7

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -84,64 +84,6 @@ function Contact() {
                       >
                         (+91) 98254 71959  | 
                       </a>
-    return (
-        <>
-            <section>
-                <div className='full-width'>
-                    <div className="main-content-1320">
-                        <div className="padding-dft"></div>
-                        <div className='contact-container'>
-                            <div className="title contact-title">
-                                <p className="maintitle">Contact Us</p>
-                            </div>
-                            <div className="contact-content">
-                                <div className="contact-form">
-                                    <div className="name-contact-row">
-                                        <div className="input-field name-contact">
-                                            <div className='input-field'>Name</div>
-                                            <div className="input-group name-contact-input">
-                                                <input type="text" className='input' />
-                                            </div>
-                                        </div>
-                                        <div className="input-field name-contact">
-                                            <div className='input-field'>Contact Number</div>
-                                            <div className="input-group name-contact-input">
-                                                <input type="text" className='input' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="email-row">
-                                        <div className="input-field">
-                                            <div className='input-field'>Email</div>
-                                            <div className="input-group">
-                                                <input type="text" className='input' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="message-row">
-                                        <div className="input-field">
-                                            <div className='input-field'>Message</div>
-                                            <div className="input-group">
-
-                                                <textarea type="text" className='input'/>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="btn-contact">
-                                        <Button label="Submit" />
-                                    </div>
-                                </div>
-                                <div className="contact-address">
-                                    <div className="address">
-                                        <img src={locationicon} alt="location-icon" className='icon-contact' />
-                                        <p className='address-land-mark'>
-                                        <a href="https://goo.gl/maps/npGPYMXJBweg7XJX6" target="_blank" rel="nopper" className="contact-decoration">A9, Gauri Nagar Society, Near Swaminarayan Temple, Ranna Park, Ghatlodia, Ahmedabad- 380061</a>
-                                        </p>
-                                    </div>
-                                    <div className="phone-number">
-                                        <img src={phoneicon} alt="phone-icon" className='icon-contact' />
-                                        <p className='address-land-mark'>
 
                       <a href="tel:7016141186" className="contact-decoration">
                         (+91) 7016141186
