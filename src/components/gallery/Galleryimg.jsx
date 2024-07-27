393b17f8fc993ec03aa5e692853513a7c3bcc274
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-07-27 19:13:07 +0530

    commit 27-07

--- src/components/gallery/Galleryimg.jsx
+++ src/components/gallery/Galleryimg.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import gimg from "../../images/gimg.png"
+import React from "react";
+import gimg from "../../images/gimg.png";
 import "./gallery.css";
 const Galleryimg = () => {
   return (
     <>
       <section>
-        <div className='full-width'>
+        <div className="full-width">
           <div className="main-content-1320">
             <div className="padding-dft"></div>
 
             <div className="gallery-padding">
               <div className="title">
-                <p className="maintitle">
-                  DFT Alumni Family Meet 2023 Images
-                </p>
+                <p className="maintitle">DFT Alumni Family Meet 2023 Images</p>
               </div>
               <div className="gimg-container">
                 <div>
@@ -25,11 +23,17 @@ const Galleryimg = () => {
                 <div>
                   <img src={gimg} alt="" srcset="" className="gimgs" />
                 </div>
-              </div>
 
+                <div>
+                  <img src={gimg} alt="" srcset="" className="gimgs" />
+                </div>
+                <div>
+                  <img src={gimg} alt="" srcset="" className="gimgs" />
+                </div>
+                <div>
+                  <img src={gimg} alt="" srcset="" className="gimgs" />
+                </div>
 
-
-              <div className="gimg-container">
                 <div>
                   <img src={gimg} alt="" srcset="" className="gimgs" />
                 </div>
@@ -40,28 +44,12 @@ const Galleryimg = () => {
                   <img src={gimg} alt="" srcset="" className="gimgs" />
                 </div>
               </div>
-
-
-
-              <div className="gimg-container">
-                <div>
-                  <img src={gimg} alt="" srcset="" className="gimgs" />
-                </div>
-                <div>
-                  <img src={gimg} alt="" srcset="" className="gimgs" />
-                </div>
-                <div>
-                  <img src={gimg} alt="" srcset="" className="gimgs" />
-                </div>
-              </div>
-
-
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Galleryimg
+export default Galleryimg;
