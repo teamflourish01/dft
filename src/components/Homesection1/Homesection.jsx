f8d61a547a170a425b9d80abac7438efd15b1716
RinaBhalani <rina.teamflourish@gmail.com>
2024-07-29 11:00:02 +0530

    add real content

--- src/components/Homesection1/Homesection.jsx
+++ src/components/Homesection1/Homesection.jsx
@@ -11,45 +11,46 @@ import welcome from "../../images/welcome.png";
 import homebanner from "../../images/homebanner.png";
 
 const Homesection = () => {
-    useEffect(() => {
-        AOS.init({ duration: 2000 });
-      }, []);
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <>
       <section>
         <div className="full-width">
-        <div className="width-img" data-aos="slide-right"></div>
-        <div className="full-widths" data-aos="fade-left">
-          <div className="content-flex">
-            <div className="main-content-1320">
-              <div className="padding-dft">
-                <div className="adventure">
-                  <div className="adventure-main">
-                    <div className="adventure-flex">
-                      <div className="adventure-left" data-aos="slide-right">
-                        <p className="find">FIND YOUR ADVENTURE</p>
-                      
-                        <p className="let">LET’S GO!</p>
-                        <p className="amet">
-                          Lorem ipsum dolor sit amet, consectetuer adipiscing elit
-                          sed diam nonummy nibh euismod tincidunt ut laoreet
-                        </p>
-                        <p className="nonummy">
-                          
-                        </p>
-                      </div>
-                      <div className="adventure-right">
-                        <div className="adv-flex">
-                          <div className="adv-left" data-aos="slide-right">
-                            <img src={homebanner} alt="Home Banner"  className="home-banner"/>
-                            
-                          </div>
-                          <div className="adv-right" data-aos="slide-left">
-                            <div className="img-animation">
-                              <img src={dft} alt="DFT" className="dft-image"/>
+          <div className="width-img" data-aos="slide-right"></div>
+          <div className="full-widths" data-aos="fade-left">
+            <div className="content-flex">
+              <div className="main-content-1320">
+                <div className="padding-dft">
+                  <div className="adventure">
+                    <div className="adventure-main">
+                      <div className="adventure-flex">
+                        <div className="adventure-left" data-aos="slide-right">
+                          <p className="find">To the Path of Success</p>
+
+                          <p className="let"> Be a part of us</p>
+                          <p className="amet">
+                            Create your Future with Diploma in Fabrication Technology. 
+                            Learn, Apply, and Be Successful. Start your Journey with DFT.
+                          </p>
+                          <p className="nonummy">
+
+                          </p>
+                        </div>
+                        <div className="adventure-right">
+                          <div className="adv-flex">
+                            <div className="adv-left" data-aos="slide-right">
+                              <img src={homebanner} alt="Home Banner" className="home-banner" />
+
                             </div>
-                            <div className="img-animation">
-                              <img src={welcome} alt="Welcome"  className="welcome-img"/>
+                            <div className="adv-right" data-aos="slide-left">
+                              <div className="img-animation">
+                                <img src={dft} alt="DFT" className="dft-image" />
+                              </div>
+                              <div className="img-animation">
+                                <img src={welcome} alt="Welcome" className="welcome-img" />
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -58,10 +59,9 @@ const Homesection = () => {
                   </div>
                 </div>
               </div>
+
             </div>
-            
-            </div>
-            </div>
+          </div>
         </div>
       </section>
     </>
